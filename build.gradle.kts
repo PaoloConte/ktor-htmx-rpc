cb5bd57e623e8e5597d4e164e8c34965a7a57f78
Paolo Conte <paolo.conte@gmail.com>
2026-03-07 08:38:40 +0100

    build: publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,19 @@
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.dokka)
 }
 
 allprojects {
     repositories {
         mavenCentral()
     }
+}
+
+
+dependencies {
+    dokka(project(":lib"))
+}
+
+dokka {
+    moduleName.set("kotlin-htmx-rpc")
 }
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,14 +1,61 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.maven.publish)
+    alias(libs.plugins.dokka)
 }
 
-group = "com.example"
-version = "1.0-SNAPSHOT"
+group = "io.paoloconte"
+version = "1.0"
 
 kotlin {
     jvmToolchain(11)
 }
+repositories {
+    mavenCentral()
+}
 
 dependencies {
     implementation(libs.ktor.server.core)
+}
+
+
+configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
+    configure(
+        KotlinJvm(
+            javadocJar = JavadocJar.Dokka("dokkaGeneratePublicationHtml"),
+            sourcesJar = true,
+        )
+    )
+
+    pom {
+        inceptionYear.set("2026")
+        url.set("https://github.com/PaoloConte/ktor-htmx-rpc")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://github.com/PaoloConte/ktor-htmx-rpc/blob/main/LICENSE")
+                distribution.set("https://github.com/PaoloConte/ktor-htmx-rpc/blob/main/LICENSE")
+            }
+        }
+        developers {
+            developer {
+                id.set("PaoloConte")
+                name.set("Paolo Conte")
+                url.set("https://github.com/PaoloConte/")
+            }
+        }
+        scm {
+            url.set("https://github.com/PaoloConte/ktor-htmx-rpc")
+            connection.set("scm:git:git@github.com:PaoloConte/ktor-htmx-rpc.git")
+            developerConnection.set("scm:git:ssh://git@github.com/PaoloConte/ktor-htmx-rpc.git")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
 }
